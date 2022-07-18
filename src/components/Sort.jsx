4e102bb709bdbc08a96daea4f12165cd81c57e0c
ShadePrima <aleksandrag1984@gmail.com>
2022-07-18 16:35:01 +0300

    added sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-function Sort() {
+function Sort({ value, onChangeSort }) {
   const [open, setOpen] = React.useState(false);
-  const [selected, setSelected] = React.useState(0);
-  const list = ["popularity", "price", "alphabetical"];
-  const sortName = list[selected];
+  const list = [
+    { name: "popularity (desc)", sortProperty: "rating" },
+    { name: "popularity (asc)", sortProperty: "-rating" },
+    { name: "price (desc)", sortProperty: "price" },
+    { name: "price (asc)", sortProperty: "-price" },
+    { name: "alphabetical (desc)", sortProperty: "title" },
+    { name: "alphabetical (asc)", sortProperty: "-title" },
+  ];
 
   const onClickListItem = (index) => {
-    setSelected(index);
+    onChangeSort(index);
     setOpen(false);
   };
 
@@ -27,18 +32,20 @@ function Sort() {
           />
         </svg>
         <b>Sort by:</b>
-        <span onClick={() => setOpen(!open)}>{sortName}</span>
+        <span onClick={() => setOpen(!open)}>{value.name}</span>
       </div>
       {open && (
         <div className="sort__popup">
           <ul>
-            {list.map((name, index) => (
+            {list.map((obj, index) => (
               <li
                 key={index}
-                onClick={() => onClickListItem(index)}
-                className={selected === index ? "active" : ""}
+                onClick={() => onClickListItem(obj)}
+                className={
+                  value.sortProperty === obj.sortProperty ? "active" : ""
+                }
               >
-                {name}
+                {obj.name}
               </li>
             ))}
           </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,38 @@ import Sort from "../components/Sort";
 const Home = (props) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "popularity",
+    sortProperty: "rating",
+  });
 
   React.useEffect(() => {
-    fetch("https://62cc78498042b16aa7cff6cc.mockapi.io/items")
+    setIsLoading(true);
+
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://62cc78498042b16aa7cff6cc.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(index) => setCategoryId(index)}
+        />
+        <Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
       </div>
       <h2 className="content__title">All pizzas</h2>
       <div className="content__items">
